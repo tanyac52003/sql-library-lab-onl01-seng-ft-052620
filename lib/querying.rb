c79d955d2610c92d428f92fd5e04e6cfd61291a1
Tanya Hodges <tanyac52003@gmail.com>
2020-06-24 13:58:27 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -26,7 +26,12 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title 
-  FROM series 
+  FROM series
+  JOIN books ON books.series_id = series_id
+  JOIN character_books ON character_books.books_id = books.id 
+  JOIN characters ON character_books.id = characters.id 
+  WHERE characters.species = 'human' GROUP BY series.title 
+  ORDER BY COUNT(*) DESC LIMIT
   "
 end
 

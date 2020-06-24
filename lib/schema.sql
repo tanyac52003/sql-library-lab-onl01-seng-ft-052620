0894efb16cc180067569ee2bc9307810d2b3eac4
Tanya Hodges <tanyac52003@gmail.com>
2020-06-24 12:38:14 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,1 +1,20 @@
-CREATE TABLE characters();
+CREATE TABLE characters(
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  species TEXT,
+  motto TEXT,
+  author_id INTEGER
+);
+
+CREATE TABLE books(
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  year INTEGER,
+  series_id INTEGER
+);
+
+CREATE TABLE series();
+
+CREATE TABLE authors();
+
+CREATE TABLE subgenres();

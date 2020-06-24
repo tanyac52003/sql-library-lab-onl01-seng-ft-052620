7cddb8eb8176cdfe798fbf430362e7c57a47a6c1
Tanya Hodges <tanyac52003@gmail.com>
2020-06-24 13:29:46 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,14 +1,20 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT books.title, books.year FROM books WHERE series_id = 1 ORDER BY books.year;"
+  "SELECT books.title, books.year 
+  FROM books WHERE series_id = 1 
+  ORDER BY books.year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT characters.name, characters.motto FROM characters ORDER BY motto ASC LIMIT 1;"
+  "SELECT characters.name, characters.motto 
+  FROM characters 
+  ORDER BY motto ASC LIMIT 1;"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species, COUNT (*) FROM characters GROUP BY species ORDER BY COUNT species DESC LIMIT 1;"
+  "SELECT species, COUNT (*) 
+  FROM characters 
+  GROUP BY species ORDER BY COUNT(species) DESC LIMIT 1;"
 end 
 
 def select_name_and_series_subgenres_of_authors

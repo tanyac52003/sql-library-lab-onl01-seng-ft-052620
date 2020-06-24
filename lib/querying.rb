93477ad9c45a391ddb5f30e820eca4aecbf5c50b
Tanya Hodges <tanyac52003@gmail.com>
2020-06-24 13:41:46 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,7 +20,7 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name 
   FROM authors
-  JOIN series ON series.author_id = authors.author_id
+  JOIN series ON series.author_id = authors.id
   JOIN subgenres ON series.subgenres_id = subgenres.id;"
 end
 

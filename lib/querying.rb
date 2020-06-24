a0dc4e8cb028ce3672c1e5e031e03dcf6580537b
Tanya Hodges <tanyac52003@gmail.com>
2020-06-24 14:11:46 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -25,15 +25,14 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title 
-  FROM series
-  JOIN books ON books.series_id = series_id
-  JOIN character_books ON character_books.book_id = books.id 
-  JOIN characters ON character_books.id = characters.id 
-  WHERE characters.species = 'human' GROUP BY series.title 
-  ORDER BY COUNT(*) DESC LIMIT 1;"
+  "SELECT series.title FROM series 
+  JOIN characters ON series.id = characters.series_id 
+  WHERE characters.species = 'human' LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name"
+  "SELECT characters.name, COUNT(character_books.character_id)
+  FROM characters 
+  JOIN character_books ON characters.id = character_books.character_id
+  GROUP BY characters.name ORDER BY COUNT(characters.id)"
 end

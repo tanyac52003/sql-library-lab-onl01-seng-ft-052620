d83f07b65e5b923d92ab0a807c8a435c13b5e1c6
Tanya Hodges <tanyac52003@gmail.com>
2020-06-24 12:40:46 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -14,9 +14,17 @@ CREATE TABLE books(
 );
 
 CREATE TABLE series(
-  
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  author_id TEXT,
+  subgenre_id TEXT
 );
 
-CREATE TABLE authors();
+CREATE TABLE authors(
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
 
-CREATE TABLE subgenres();
+CREATE TABLE subgenres(
+  
+);

a96c750b0dbc1f5e6c64fa8ef58098b7ebba2be0
Tanya Hodges <tanyac52003@gmail.com>
2020-06-24 13:09:50 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,7 +8,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  ""
+  "SELECT characters.species FROM characters SUM(characters.species) GROUP BY "
 end
 
 def select_name_and_series_subgenres_of_authors
